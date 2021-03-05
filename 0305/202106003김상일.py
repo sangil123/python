67bfacbe84dbebfb39acde75d91f6e7946f19fce
sangil123 <80086353+sangil123@users.noreply.github.com>
2021-03-05 16:02:08 +0900

    Add files via upload

---
+++ 0305/202106003김상일.py
@@ -0,0 +1,7 @@
+#학번을 입력받아 stdNum이라는 변수에 저장합니다.
+stdNum = input("학번 : ")
+#이름을 입력받아 stdName이라는 변수에 저장합니다.
+stdName = input("이름 : ")
+#학번과 이름을 출렵합니다.
+print("당신의 학번은 " ,stdNum,"이고 이름은", stdName,"입니다.")
+
