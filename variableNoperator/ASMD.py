10dffb9f11a6d79361545aee70905c182082ad47
sangil123 <80086353+sangil123@users.noreply.github.com>
2021-03-12 16:06:25 +0900

    Add files via upload

---
+++ variableNoperator/ASMD.py
@@ -0,0 +1,21 @@
+#두 정수를 입력받아 사칙연산 결과를 출력 
+a= int(input("첫 번째 숫자 : "))
+b= int(input("두 번째 숫자 : "))
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)
+
+a=int(input("첫 번째 숫자"))
+b=int(input("두 번째 숫자"))
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)
+
+a=int(input("첫 번째 숫자: "))
+b=int(input("두 번째 숫자: "))
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)
